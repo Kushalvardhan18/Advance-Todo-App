0604ca56f71daf0a0f82aea4fc8d1c4ccd1f7f84
Kushal Vardhan <vardhank140102@gmail.com>
2025-03-14 19:12:39 +0530

    functionality completed

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const createBoard = document.querySelector(".createBoard")
 const colorBoard = document.querySelector("#newColor")
 let taskInput = document.querySelector("#text")
 let boardName = document.querySelector("#boardName")
-let taskDescription = document.querySelector('#description')
 let deadlineDate = document.querySelector("#date")
 const submitTask = document.querySelector("#submit")
 const createNewBoard = document.querySelector("#createNewBoard")
@@ -25,19 +24,16 @@ createBoard.addEventListener('click', () => {
 
 let input = ""
 let deadline = ""
+
 taskInput.addEventListener('input', (e) => {
     input = e.target.value
-})
-
-taskDescription.addEventListener('input', (e) => {
-    console.log(e.target.value);
-
 })
 
 deadlineDate.addEventListener('input', (e) => {
     deadline = e.target.value
 
 })
+
 let newBoardName = ""
 boardName.addEventListener('input', (e) => {
     newBoardName = e.target.value
@@ -59,24 +55,59 @@ function attachDragEvents(target) {
 
 submitTask.addEventListener('click', () => {
     if (!taskInput.value.trim()) return
+
     const newTaskTitle = document.createElement("h3")
     newTaskTitle.classList.add('todo')
     newTaskTitle.innerText = input.toUpperCase()
+
     const clrItem = document.querySelector("#task-todo")
+    const dateDeadline = document.createElement("span")
+    const deleteTask = document.createElement("button")
+    const accordionDiv = document.createElement("div")
+    deleteTask.innerText = "X"
+    dateDeadline.innerText = deadlineDate.value
     newTaskTitle.style.color = clrItem.value
     newTaskTitle.draggable = true
-    newTaskTitle.setAttribute("deadlineDate", deadline)
+    accordionDiv.classList.add("accordionContent")
+     accordionDiv.style.display = "none"
+
+    deleteTask.addEventListener("click", () => {
+        deleteTaskFn(newTaskTitle)
+    })
+    newTaskTitle.addEventListener("click", () => {
+        showAccordionFn(dateDeadline, accordionDiv)
+    })
+
     newTodos.appendChild(newTaskTitle)
+    newTaskTitle.appendChild(accordionDiv)  
+    accordionDiv.appendChild(dateDeadline) 
+    newTaskTitle.appendChild(deleteTask)
     todoBoard.appendChild(newTodos)
+
     attachDragEvents(newTaskTitle)
+
+
 
     // clearing the form inputs
     taskForm.reset()
     todoForm.close()
     taskInput.value = ""
     input = ""
+    deadline = ""
+    deadlineDate.value = ""
 
 })
+
+function showAccordionFn(deadline, accordion) {
+    if (accordion.style.display === "none") {
+        accordion.style.display = "block"  
+    } else {
+        accordion.style.display = "none"
+    }
+}
+function deleteTaskFn(task) {
+    task.remove()
+}
 
 cancelCreation.addEventListener('click', () => {
     if (!taskInput) {
@@ -101,10 +132,10 @@ createNewBoard.addEventListener('click', () => {
     newBoardDiv.appendChild(inputClr)
     newBoardDiv.appendChild(heading)
     boards.appendChild(newBoardDiv)
-    
+
     inputClr.addEventListener('change', (e) => {
         const board = e.target.closest(".board")
-        
+
         if (board) {
             const allTodos = board.querySelectorAll(".todo")
             allTodos.forEach((todo) => {
@@ -112,7 +143,7 @@ createNewBoard.addEventListener('click', () => {
             })
         }
     })
-   
+
 
     // attach dragover
 
@@ -127,14 +158,14 @@ createNewBoard.addEventListener('click', () => {
             newBoardDiv.appendChild(draggableElement)
         }
     })
-    
+
     boardName.value = ""
     newBoardName = ""
-    colorBoard.value = ""
+    colorBoard.style.color = "#000000"
     newBoardColor = ""
     boardInfo.close()
-    boardInfo.reset()
-    attachDragEvents()
+    // attachDragEvents(newBoardDiv)
+    // boardInfo.reset()
 })
 
 dismissNewBoardCreation.addEventListener('click', () => {
@@ -143,10 +174,10 @@ dismissNewBoardCreation.addEventListener('click', () => {
     }
     boardName.value = ""
     newBoardName = ""
-    colorBoard.value = ""
+    colorBoard.style.color = "#000000"
     newBoardColor = ""
     boardInfo.close()
-    boardInfo.reset()
+    // boardInfo.reset()
 })
 
 const allTodos = document.querySelectorAll(".todo")
@@ -170,7 +201,6 @@ allBoards.forEach((board) => {
 
 
 const clrTodoChange = document.querySelectorAll(".clr-todo");
-
 clrTodoChange.forEach((item) => {
     item.addEventListener('change', (e) => {
         const board = e.target.closest(".board")
@@ -183,3 +213,4 @@ clrTodoChange.forEach((item) => {
         }
     })
 })
+
